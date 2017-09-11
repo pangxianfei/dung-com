a0db2b1cf028fcc55972ead4f7c160feac469531
庞贤飞 <421339244@qq.com>
2017-09-11 12:43:33 +0800

    项目建立

---
+++ DUNGU.COM/DUNGU.COM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DUNGU.COM
+//
+//  Created by 庞贤飞 on 2017/9/11.
+//  Copyright © 2017年 庞贤飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DUNGU.COM/DUNGU.COM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DUNGU.COM
+//
+//  Created by 庞贤飞 on 2017/9/11.
+//  Copyright © 2017年 庞贤飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
